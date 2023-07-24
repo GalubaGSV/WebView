eabedf0c73780d368955a60346ff1bd45f143f3e
Sergiy Golubchenko <galuba1@gmail.com>
2023-07-24 21:02:21 +0300

    first commit

---
+++ app/src/main/java/com/example/webview/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.webview
+
+import android.annotation.TargetApi
+import android.os.Build
+import android.os.Bundle
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webView : WebView = findViewById(R.id.webView)
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl("https://youtube.com")
+
+        val webViewClient: WebViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+                view.loadUrl(url)
+                return true
+            }
+
+            @TargetApi(Build.VERSION_CODES.N)
+            override fun shouldOverrideUrlLoading(
+                view: WebView,
+                request: WebResourceRequest
+            ): Boolean {
+                view.loadUrl(request.url.toString())
+                return true
+            }
+        }
+        webView.webViewClient = webViewClient
+    }
+}
